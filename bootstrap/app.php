ebfa6f5e01f76f180483cb0cce894b226b15af59
begimov <begimov@gmail.com>
2018-02-20 16:07:13 +0300

    illuminate/database set up, db settings added to container config via env parameters

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -18,11 +18,27 @@ $app = new Slim\App([
 
         'views' => [
             'cache' => getenv('VIEW_CACHE_DISABLED') === 'true' ? false : __DIR__ . '/../storage/views'
+        ],
+
+        'db' => [
+            'driver' => getenv('DB_DRIVER'),
+            'host' => getenv('DB_HOST'),
+            'database' => getenv('DB_DATABASE'),
+            'username' => getenv('DB_USERNAME'),
+            'password' => getenv('DB_PASSWORD'),
+            'charset' => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix' => '',
         ]
     ],
 ]);
 
 $container = $app->getContainer();
+
+$capsule = new Illuminate\Database\Capsule\Manager;
+$capsule->addConnection($container['settings']['db']);
+$capsule->setAsGlobal();
+$capsule->bootEloquent();
 
 $container['view'] = function ($container) {
     $view = new \Slim\Views\Twig(__DIR__ . '/../resources/views', [
