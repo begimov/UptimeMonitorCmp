3491665708ead5433dfc59b5bfe703fd05804883
begimov <begimov@gmail.com>
2018-02-21 15:58:26 +0300

    class Run for command created

---
+++ app/Console/Commands/Run.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Endpoint;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class Run extends Command
+{
+    protected function configure()
+    {
+        $this->setName('run');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        //
+    }
+}
