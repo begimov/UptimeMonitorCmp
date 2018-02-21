49c5942d2bd37fa07cfeb5e096631e0106c78a99
begimov <begimov@gmail.com>
2018-02-21 14:52:09 +0300

    frequency option added to endpoint:add console command

--- app/Console/Commands/AddEndpoint.php
+++ app/Console/Commands/AddEndpoint.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands;
 
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -13,7 +14,8 @@ class AddEndpoint extends Command
     {
         $this->setName('endpoint:add')
             ->setDescription('Adds new endpoint to UMonitor to watch for')
-            ->addArgument('endpoint', InputArgument::REQUIRED);
+            ->addArgument('endpoint', InputArgument::REQUIRED, 'Endpoint URL')
+            ->addOption('frequency', 'f', InputOption::VALUE_OPTIONAL, 'Number of endpoint checks per minute', 1);
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
